68f266814e16a8a5bfb8a4a4a0b4a3b1bf781bf2
mbotos2000 <81249560+mbotos2000@users.noreply.github.com>
2025-06-24 12:58:50 +0300

    Update fisa_din_baza.py

--- fisa_din_baza.py
+++ fisa_din_baza.py
@@ -632,11 +632,11 @@ if not(st.session_state['ut']):
     if st.session_state['file']!=None or st.session_state['ut']:
         if st.session_state['file']!=None:
           try:
-           data_fis = load_pkl_from_ftp('./public_html/Fise/2025/'+st.session_state['file'])      
-          except:
-           data_fis = load_pkl_from_ftp('./public_html/Fise/2024/'+st.session_state['file'])   
-          #except Exception as e:
-          #  st.error(f"Error loading file: {e}")
+           data_fis = load_pkl_from_ftp('./public_html/Fise/20'+st.session_state['file'][-6:-5]+'/'+st.session_state['file'])      
+          #except:
+          # data_fis = load_pkl_from_ftp('./public_html/Fise/2024/'+st.session_state['file'])   
+          except Exception as e:
+            st.error(f"Error loading file: {e}")
 
         
           nume_tit = data['nume'].tolist()
